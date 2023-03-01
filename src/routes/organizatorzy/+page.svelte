b7f36a182e759e6dc7e60a9668cf5a1c9672c99b
Mareczekk2137 <Markos.Jamrozik@gmail.com>
2023-03-01 22:30:38 +0100

    add space for description under organizers

--- src/routes/organizatorzy/+page.svelte
+++ src/routes/organizatorzy/+page.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div
-	style="grid-template-columns: repeat(auto-fit, 13.5rem);"
+	style="grid-template-columns: repeat(auto-fit, minmax(0, 25rem));"
 	class="mt-8 grid justify-items-center justify-center"
 >
 	{#each data.items as user, i}
-		<div class="flex flex-col w-52 items-center">
+		<div class="flex flex-col items-center">
 			<img
 				in:fly={{ y: 10, duration: 700, delay: i * 100 }}
 				class="object-cover w-32 h-48"
@@ -18,7 +18,10 @@
 			<pc class="pt-2 h-8 text-center italic">
 				{user.imie + ' ' + user.nazwisko}
 			</pc>
-			<p class=" pt-4 text-center text-sm ">
+			<p class=" pt-4 text-center text-sm h-10">
+				{user.rola}
+			</p>
+			<p class=" pt-4 text-center text-xs pb-4">
 				{user.opis}
 			</p>
 		</div>
